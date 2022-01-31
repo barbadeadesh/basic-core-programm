695efb86acc7da36e513e1650c5cba5248c8576d
barbadeadesh <barbadeadesh2020@email.com>
2022-01-31 23:02:58 +0530

    coin is flip

---
+++ Flipcoin.java
@@ -0,0 +1,15 @@
+package com.corejava;
+
+public class Flipcoin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        if(Math.random()<0.5)
+            System.out.println("Head");
+        else
+            System.out.println("Tail");
+
+	}
+
+}
