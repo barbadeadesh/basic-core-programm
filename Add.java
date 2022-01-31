a60a0f65438bf28174034bbc63d9973734b0434c
barbadeadesh <barbadeadesh2020@email.com>
2022-01-31 22:59:28 +0530

    addition of two no

---
+++ Add.java
@@ -0,0 +1,13 @@
+package com.corejava;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=5,b=5,sum;
+	        sum=a+b;
+	        System.out.println(sum);
+
+	}
+
+}
